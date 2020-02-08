ff338014a3a657edaf8af1c171e10043e49cb0fc
lukexyz <lukedwoods@gmail.com>
2020-02-08 20:00:39 +0000

    Titles and misc text

---
+++ app_examples.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+# Titles
+st.title("Streamlit Examples")
+st.header("This is a header")
+st.subheader("This is a subheader")
+st.text("Hello streamlit")
+st.markdown("#### This is `Markdown`")
+
+# Misc text
+st.success("Successful")
+st.info("Information")
+st.warning("This is a warning!")
+st.error("This is an error Danger")
+st.exception("NameError()")
+
+
+
+
+"""
+
+> streamlit run app.py
+
+"""
