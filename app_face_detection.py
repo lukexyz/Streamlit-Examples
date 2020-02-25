5f2d1cb7cb4da92c51d393768e1eede8467bfb55
lukexyz <lukedwoods@gmail.com>
2020-02-25 10:47:37 +0000

    Add face detection

--- app_face_detection.py
+++ app_face_detection.py
@@ -6,9 +6,24 @@ import os
 import time
 
 @st.cache
-def load_image(img):
-    return Image.open(img)
+def load_image(image_file):
+    # img = Image.open(image_file)
+    # img_out = cv2.resize(img, None, fx=0.25, fy=0.25)
+    return Image.open(image_file)
 
+face_cascade = cv2.CascadeClassifier('data/cascades/haarcascade_frontalface_alt.xml')
+
+@st.cache
+def detect_faces(our_image):
+    new_img = np.array(our_image.convert('RGB'))
+    img = cv2.cvtColor(new_img, 1)
+    bw = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+    # Detect Face
+    faces = face_cascade.detectMultiScale(bw, 1.1, 4)
+    # Draw Rectangle
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+    return img, faces
 
 def main():
     """Face Detection App"""
@@ -54,6 +69,16 @@ def main():
             img = cv2.cvtColor(new_img,1)
             blur_img = cv2.GaussianBlur(img,(11,11), b_rate)
             st.image(blur_img)
+
+        # Face Detection
+        task = ['None', 'Faces', 'Smiles', 'Eyes', 'Cannize']
+        feature_choice = st.sidebar.selectbox("Find Features", task)
+        if st.button('Find Features'):
+
+            if feature_choice == 'Faces':
+                result_img, result_faces = detect_faces(our_image)
+                st.image(result_img)
+
 
 
     elif choice == 'About':
