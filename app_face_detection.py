7f2f4d9a0c34372957959d2bf88ca2beec128799
lukexyz <lukedwoods@gmail.com>
2020-02-25 10:05:11 +0000

    Add blurring and brightness

--- app_face_detection.py
+++ app_face_detection.py
@@ -12,7 +12,7 @@ def load_image(img):
 
 def main():
     """Face Detection App"""
-
+    loader = True
     st.title("Face Detection in the Browser")
     st.text('Built with `Streamlit` and `OpenCV`')
 
@@ -24,18 +24,16 @@ def main():
         image_file = st.file_uploader("Upload Image", type=['jpg', 'png', 'jpeg'])
         
         if image_file:
-            my_bar = st.progress(0)
-            for percent_complete in range(100):
-                time.sleep(0.01)
-                my_bar.progress(percent_complete + 1)
             st.text('File uploaded.')
-
             our_image = Image.open(image_file)
             st.text("Original Image")
             st.image(our_image)
+            
 
 
-        enhance_type = st.sidebar.radio("Enhance Type", ["Original", "Gray-Scale", "Contrast", "Brightness"])
+        enhance_type = st.sidebar.radio("Enhance Type", 
+                       ["Original", "Gray-Scale", "Contrast", "Brightness", "Blurring"])
+        
         if enhance_type == 'Gray-Scale':
             new_img = np.array(our_image.convert(('RGB')))
             bw = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
@@ -45,6 +43,17 @@ def main():
             enhancer = ImageEnhance.Contrast(our_image)
             img_output = enhancer.enhance(c_rate)
             st.image(img_output)
+        if enhance_type == 'Brightness':
+            c_rate = st.slider("Brightness", 0.5, 3.5)
+            enhancer = ImageEnhance.Brightness(our_image)
+            img_output = enhancer.enhance(c_rate)
+            st.image(img_output)
+        if enhance_type == 'Blurring':
+            b_rate = st.slider("Blurring Rate", 0.5, 3.5)
+            new_img = np.array(our_image.convert(('RGB')))
+            img = cv2.cvtColor(new_img,1)
+            blur_img = cv2.GaussianBlur(img,(11,11), b_rate)
+            st.image(blur_img)
 
 
     elif choice == 'About':
